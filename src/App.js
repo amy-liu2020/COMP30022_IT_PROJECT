48a5cf8755d0ba272b2fe9b25524127465138233
haiyao <80369914+haiyao1073969@users.noreply.github.com>
2021-09-09 14:46:35 +1000

    add draft version of navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { Header } from './components/Header';
+import { NavigationBar } from './components/NavigationBar'
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <NavigationBar />
     </div>
   );
 }
