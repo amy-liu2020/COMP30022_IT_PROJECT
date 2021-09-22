82ea11c90ef733c7cf315217896d8d6a46ea943a
haiyao <haiyao@student.unimelb.edu.au>
2021-09-23 00:14:09 +1000

    update for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,76 @@
 import './App.css';
-import { Contact } from './components/Contact';
-import { Meeting } from './components/Meeting';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import { Contact } from './components/Contact';
+//import { Meeting } from './components/Meeting';
+// import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+// import { Login } from './components/Login';
+import { useState } from 'react';
+import axios from 'axios';
 
-const Login = () => {
-  return (
-  <div>
-    <p>Login page</p>
-  </div>)
-}
+// const Contact = () => {
+//   return (
+//   <div>
+//     <p>Contact page</p>
+//   </div>)
+// }
 
 // const Meeting = () => {
 //   return (
-//   <div className="three-part-layout">
-//     <NavigationBar/>
-//     <SideMenu groups = {groups} tab={"meeting"}/>
+//   <div>
+//     <p>Meeting page</p>
 //   </div>)
 // }
+
+const Test = () => {
+	const [selectedFile, setSelectedFile] = useState();
+	
+  // toggle when selected file changed
+  const onChangeHandler = (e) => {
+    setSelectedFile(e.target.files[0]);
+  }
+
+  const onSubmit = () => {
+    const data = new FormData() 
+    data.append('file', selectedFile);
+    console.warn(selectedFile);
+    let url = "api/uploadPhoto";
+
+    axios.post(url, data, { // receive two parameter endpoint url ,form data 
+    })
+    .then(res => { // then print response status
+        console.warn(res);
+    })
+  }
+
+  return (
+  <div>
+    <p>Login page</p>
+    <input type="file" onChange={onChangeHandler}/>
+    <button type="submit" onClick={onSubmit}>submit</button>
+  </div>)
+
+}
 
 function App() {
 
   return (
-    <Router>
-      <Switch>
-        <Route path='/contact' component={Contact}></Route>
-        <Route path='/meeting' component={Meeting}></Route>
-        <Route path='/' component={Login}></Route>
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Link to="/">home</Link>
+    //   <hr/>
+    //   <Link to="/login">login</Link>
+    //   <hr/>
+    //   <Link to="/contact">contact</Link>
+    //   <hr/>
+    //   <Link to="/meeting">meeting</Link>
+    //   <hr/>
+    //   <Link to="/meeting">meeting</Link>
+    //   <Switch>
+    //     <Route path="/contact"><Contact/></Route>
+    //     <Route path="/meeting"><Meeting/></Route>
+    //     <Route path="/login"><Login/></Route>
+    //     <Route path="/"><p>home page</p></Route>
+    //   </Switch>
+    // </Router>
+    <Test/>
   );
 }
 
--- server.js
+++ server.js
@@ -1,99 +1,123 @@
-//const mongoose = require("mongoose");
+// //const mongoose = require("mongoose");
 
-// import backend framwork
-const express = require("express");
-const session = require('express-session')
-const ejs = require("ejs");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
-const MongoStore = require('connect-mongo');
-
-
-const app = express();
-const server = require("http").createServer(app);
-const port = process.env.PORT || 5000;
-
-// link to database
-const db = require('./models/db.js')
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-app.use(session({
-    secret: "She could fade and wither - I didn't care.",
-    name: "UserAccount",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 1000 * 60 * 2,
-        secure: false
-    },
-    store: MongoStore.create({ mongoUrl: "mongodb+srv://AEHXZ:aehxz123456@cluster0.0vlpa.mongodb.net/CRM?retryWrites=true&w=majority" }),
-    rolling: true
-}))
+// // import backend framwork
+// const express = require("express");
+// const session = require('express-session')
+// const ejs = require("ejs");
+// const path = require("path");
+// const bodyParser = require("body-parser");
+// const cookieParser = require('cookie-parser');
+// const MongoStore = require('connect-mongo');
 
 
-app.set("view engine", "ejs");
+// const app = express();
+// const server = require("http").createServer(app);
+// const port = process.env.PORT || 5000;
 
-Ses = require("./models/session")
+// // link to database
+// const db = require('./models/db.js')
 
-// link to routes 
-const contactRouter = require("./routes/contactRouter");
-const meetingRouter = require("./routes/meetingRouter");
-const userRouter = require("./routes/userRouter");
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(session({
+//     secret: "She could fade and wither - I didn't care.",
+//     name: "UserAccount",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         maxAge: 1000 * 60 * 2,
+//         secure: false
+//     },
+//     store: MongoStore.create({ mongoUrl: "mongodb+srv://AEHXZ:aehxz123456@cluster0.0vlpa.mongodb.net/CRM?retryWrites=true&w=majority" }),
+//     rolling: true
+// }))
 
-// Interceptor
-app.all('*', (req, res, next) => {
-    if(req.session.loginState == 1){
-        req.session.destroy();
-        console.log("you are taken")
-        res.redirect("/")
-    }
-    next();
-})
 
-app.use("/contact", contactRouter);
-app.use("/meeting", meetingRouter);
-app.use("/", userRouter);
+// app.set("view engine", "ejs");
 
-// 'default' route to catch user errors
-app.all('*', (req, res) => {
-    res.status(404).render('404')
-})
+// Ses = require("./models/session")
 
-//Indicating running backend
-app.listen(port, () => {
-    console.log(`App is running at ${port}`)
-})
+// // link to routes 
+// const contactRouter = require("./routes/contactRouter");
+// const meetingRouter = require("./routes/meetingRouter");
+// const userRouter = require("./routes/userRouter");
 
-exports.conflictLoginCheck = async function (req, res) {
-    let strs = await Ses.find({}, async function (err, doc) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        return doc;
-    });
-    for (i = 0; i < strs.length; i++) {
-        let keywords = strs[i].session.split(/"/);
-        for (j = 0; j < keywords.length - 2; j++) {
-            if (keywords[j] == "userid" && keywords[j + 2] == req.session.userid) {
-                if (strs[i]._id == req.session.id) {
-                    return false;
-                } else {
-                    Ses.findOneAndUpdate({ _id: strs[i]._id }, {loginState: 1}, function (err, doc) {
-                        if (err) {
-                            console.log(err);
-                            return;
-                        }
-                    })
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
+// // Interceptor
+// app.all('*', (req, res, next) => {
+//     if(req.session.loginState == 1){
+//         req.session.destroy();
+//         console.log("you are taken")
+//         res.redirect("/")
+//     }
+//     next();
+// })
+
+// app.use("/contact", contactRouter);
+// app.use("/meeting", meetingRouter);
+// app.use("/", userRouter);
+
+// // 'default' route to catch user errors
+// app.all('*', (req, res) => {
+//     res.status(404).render('404')
+// })
+
+// //Indicating running backend
+// app.listen(port, () => {
+//     console.log(`App is running at ${port}`)
+// })
+
+// exports.conflictLoginCheck = async function (req, res) {
+//     let strs = await Ses.find({}, async function (err, doc) {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//         return doc;
+//     });
+//     for (i = 0; i < strs.length; i++) {
+//         let keywords = strs[i].session.split(/"/);
+//         for (j = 0; j < keywords.length - 2; j++) {
+//             if (keywords[j] == "userid" && keywords[j + 2] == req.session.userid) {
+//                 if (strs[i]._id == req.session.id) {
+//                     return false;
+//                 } else {
+//                     Ses.findOneAndUpdate({ _id: strs[i]._id }, {loginState: 1}, function (err, doc) {
+//                         if (err) {
+//                             console.log(err);
+//                             return;
+//                         }
+//                     })
+//                     return true;
+//                 }
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+
+var app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
