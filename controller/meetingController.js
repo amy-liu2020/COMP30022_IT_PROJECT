237997ed986de257e6c299996e966f89eb345368
XintaoChen <88045261+XintaoChen@users.noreply.github.com>
2021-10-22 20:19:32 +1100

    Update meetingController.js

--- controller/meetingController.js
+++ controller/meetingController.js
@@ -1,6 +1,7 @@
 const Bin = require("../models/bin");
 const Meeting = require("../models/meeting");
 const Tag = require("../models/tag")
+const Contact = require("../models/contact");
 
 const fs = require('fs');
 const mongoose = require("mongoose")
